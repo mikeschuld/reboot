c08dc854cbcbfd50dc7e6bf8054438ab14a62157
Michael Schuld <mikeschuld@gmail.com>
2016-11-25 23:58:59 -0700

    Initial gridfree code in place including active resizing. Needs optimization if possible

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,22 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				files: {
-					'css/rb-core.css': 'sass/rb-core.scss'
+					'css/rb-core.css': 'sass/rb-core.scss',
+					'css/rb-gridfree.css': 'sass/rb-gridfree.scss'
+				}
+			}
+		},
+
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'css/normalize.min.css': ['node_modules/normalize.css/normalize.css'],
+					'css/rb-core.min.css': ['css/rb-core.css'],
+					'css/rb-gridfree.min.css': ['css/rb-gridfree.css']
 				}
 			}
 		},
@@ -13,12 +28,14 @@ module.exports = function(grunt) {
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass']
+				tasks: ['sass', 'cssmin']
 			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	
 	grunt.registerTask('default', ['watch']);
 }
