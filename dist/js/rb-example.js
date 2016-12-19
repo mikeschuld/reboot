93009516846e9d1b06d9667fb312d5bf7faa5f9f
Michael Schuld <mikeschuld@gmail.com>
2016-12-19 01:30:15 -0700

    Added tables and example rendering code

---
+++ dist/js/rb-example.js
@@ -0,0 +1,31 @@
+"use strict";
+var _ = require("lodash");
+$(function () {
+    var $examples = $("[data-example]");
+    $examples.each(function (t, example) {
+        var $exampleId = $(example).data("example");
+        var $html = $($exampleId).html();
+        // trim whitespace from the end of the html
+        $html = $html.replace(/\s*$/, "");
+        // strip out shared initial tabs to left justify html
+        var lines = $html.split("\n");
+        lines = _.without(lines, "");
+        var match = true;
+        while (match) {
+            var stripped = new Array(lines.length);
+            for (var l = 0; l < lines.length; l++) {
+                if (lines[l].indexOf("\t") === 0) {
+                    stripped[l] = lines[l].substr(1);
+                }
+                else {
+                    match = false;
+                    stripped = lines;
+                    break;
+                }
+            }
+            lines = stripped;
+        }
+        $(example).text(_.join(lines, "\n"));
+    });
+});
+//# sourceMappingURL=rb-example.js.map
--- reboot.ts
+++ reboot.ts
@@ -4,5 +4,6 @@ import "./src/scss/rb-core.scss";
 import "./src/scss/rb-gridfree.scss";
 import "./src/scss/rb-tabs.scss";
 
+import "./src/js/rb-example.ts";
 import "./src/js/rb-gridfree.ts";
 import "./src/js/rb-tabs.ts";
