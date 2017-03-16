cddb512f50762edfd4d4a2d137f61f4fe3bfd5a6
Michael Schuld <mikeschuld@gmail.com>
2017-03-15 21:05:57 -0600

    Modal close and styling with backdrop to cover page. Needs support for stacked modals and size classes

--- src/js/rb-modals.ts
+++ src/js/rb-modals.ts
@@ -1,25 +1,53 @@
 class RBModal {
-	constructor(private $trigger: JQuery) { }
+	static open: number = 0;
+	static $backdrop: JQuery;
+
+	constructor(private $trigger: JQuery) {
+		if (RBModal.$backdrop === undefined) {
+			RBModal.$backdrop = $("<div class='rb-modal-backdrop'>");
+			RBModal.$backdrop.appendTo($("body"));
+		}
+	}
 
 	initialize(): RBModal {
-		let selector: string = this.$trigger.data("rb-modal");
+		let selector: string = this.$trigger.data("rb-modal-open");
 		let event: string = this.$trigger.data("rb-event");
 		let $modal = $(selector);
+		let $close = $modal.find("[data-rb-modal-close]");
 
 		this.$trigger.on(event, function(this: Element) {
 			$modal.trigger("modal:open");
 		});
 
-		$modal.on("modal:open", function(this: Element) {
-			$(this).addClass("rb-open");
+		$close.on("click", function(this: Element) {
+			$modal.trigger("modal:close");
 		});
+
+		$modal.on("modal:open", function(this: Element) {
+			RBModal.open++;
+			$(this).addClass("rb-open");
+
+			RBModal.$backdrop.addClass("rb-open");
+		});
+
+		$modal.on("modal:close", function(this: Element) {
+			RBModal.open--;
+			$(this).removeClass("rb-open");
+
+			if (RBModal.open <= 0) {
+				RBModal.$backdrop.removeClass("rb-open");
+			}
+		});
+
+		let $outerContainer = $modal.parents(".rb-container, .rb-container-dark").last();
+		$modal.appendTo($outerContainer);
 
 		return this;
 	}
 }
 
 $(function() {
-	$("[data-rb-modal]").each(function(this: Element) {
+	$("[data-rb-modal-open]").each(function(this: Element) {
 		let modal: RBModal = new RBModal($(this));
 		modal.initialize();
 	});
